732211d36fb9dc073e5e632e330b00e8b34b601f
FusiDaniel <fpires.daniel@gmail.com>
2023-10-24 19:11:32 -0300

    feat: rotating script

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private float baseAngle = 0.0f;
+    void OnMouseDown()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+
+        baseAngle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+        baseAngle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+    }
+    void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+        float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - baseAngle;
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+    }
+}
